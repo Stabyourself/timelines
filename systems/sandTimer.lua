42997bd078a5fa3ae887c1b6085bf2cc225552c3
Maurice <maurice@guegan.de>
2021-04-08 15:24:10 +0200

    make sand runout dependent on vx

--- systems/sandTimer.lua
+++ systems/sandTimer.lua
@@ -4,8 +4,10 @@ local sandTimer = tiny.processingSystem()
 sandTimer.filter = tiny.requireAll("sand")
 
 function sandTimer:process(e, dt)
-    if math.abs(e.vx) > 0 or math.abs(e.vy) > 0 then
-        e.sand = e.sand - dt*0.2
+    local absVX = math.abs(e.vx)
+
+    if absVX > 0 then
+        e.sand = e.sand - dt*0.2*(absVX/e.maxSpeed)
 
         if e.sand < 0 then
             e.sand = 0
